6a2a73fcaffc86433ca8f15e5a61180b6e19b69b
shepherdwind <eward.song@gmail.com>
2014-07-12 11:44:13 +0800

    add error test

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-"use strict";
-
 module.exports = function lockAndYield(fn, hashfn) {
-  hashfn = hashfn || defaultHashFn;
-  var cache = Object.create(null);
+  hashfn = hashfn || defaultHashFn
+  var cache = Object.create(null)
 
   return function* () {
 
-    var key = hashfn.apply(this, arguments);
+    var key = hashfn.apply(this, arguments)
 
     if (cache[key] && cache[key].state) {
       // in progress, wait until it's called
-      return yield await(key);
+      return yield await(key)
     }
 
-    cache[key] = { waits: [], state: true };
+    cache[key] = { waits: [], state: true }
 
-    var res = yield* fn.apply(this, arguments);
+    var res = yield* fn.apply(this, arguments)
 
     // finish waits
-    cache[key].waits.forEach(function(fn){ fn(res); });
-    delete cache[key];
+    cache[key].waits.forEach(function(fn){ fn(res) })
+    delete cache[key]
 
-    return res;
-  };
+    return res
+  }
 
   function await(key) {
     return function (done) {
       cache[key]['waits'].push(function(result){
-        done(null, result);
-      });
-    };
+        done(null, result)
+      })
+    }
   }
-};
+}
 
 function defaultHashFn() {
-  return String.call(arguments[0]);
+  return String.call(arguments[0])
 }
--- test/index.js
+++ test/index.js
@@ -24,6 +24,29 @@ describe('lock and yield', function () {
     assert.equal(calls, 1)
     assert.equal(result.join(""), "00000")
   }))
+
+  it('should work with error', co(function* () {
+    var calls = 0
+    var fn = lock(function* fn(url, timeout) {
+      calls++
+      yield wait(timeout || 10)
+
+      throw new Error("This some error happened")
+    })
+
+    try {
+      var result = yield [
+        fn('a', 0),
+        fn('a', 1),
+        fn('a', 2),
+        fn('a', 3),
+        fn('a', 4),
+      ]
+    } catch(e) {}
+
+    assert.equal(calls, 1)
+    assert.equal(result, undefined)
+  }))
 })
 
 function wait(ms) {
